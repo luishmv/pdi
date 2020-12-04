c6046b6be22706b26f6e84bd91b185abe9edb21e
Luis Henrique Matias <luysmathyas@hotmail.com>
2020-12-04 19:40:40 -0300

    Adicionada unidade2

---
+++ unidade2/codigos/kmeans_random.cpp
@@ -0,0 +1,53 @@
+#include <opencv2/opencv.hpp>
+#include <cstdlib>
+
+using namespace cv;
+
+int main( int argc, char** argv ){
+  int nClusters = 10;
+  Mat rotulos;
+  int nRodadas = 10;
+  Mat centros;
+  Mat resultados[nRodadas];
+
+  if(argc!=2) exit(0);
+
+  Mat img = imread(argv[1]);
+
+  Mat samples(img.rows * img.cols, 3, CV_32F);
+
+  for( int y = 0; y < img.rows; y++ ){
+    for( int x = 0; x < img.cols; x++ ){
+      for( int z = 0; z < 3; z++){
+        samples.at<float>(y + x*img.rows, z) = img.at<Vec3b>(y,x)[z];
+	  }
+	 }
+  }
+
+  for(uint i = 0; i < nRodadas; i++)
+  {
+    kmeans(samples,
+      nClusters,
+      rotulos,
+      TermCriteria(TermCriteria::MAX_ITER +
+                TermCriteria::EPS, 10000, 0.0001),
+      1,
+      KMEANS_RANDOM_CENTERS,
+      centros );
+
+      Mat rotulada( img.size(), img.type() );
+      for( int y = 0; y < img.rows; y++ ){
+        for( int x = 0; x < img.cols; x++ ){
+          int indice = rotulos.at<int>(y + x*img.rows,0);
+          rotulada.at<Vec3b>(y,x)[0] = (uchar) centros.at<float>(indice, 0);
+          rotulada.at<Vec3b>(y,x)[1] = (uchar) centros.at<float>(indice, 1);
+          rotulada.at<Vec3b>(y,x)[2] = (uchar) centros.at<float>(indice, 2);
+        }
+      }
+
+      rotulada.copyTo(resultados[i]);
+  }
+
+  for(uint i = 0; i < nRodadas; i++)
+    imwrite("rodada-"+std::to_string(i)+".png", resultados[i]);
+}
